da75d1e9abbcb769be653d9534a9ea0120d52207
nequalszero <nequalszero@gmail.com>
2016-10-28 21:17:05 -0700

    Phase 1 store complete

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 window.signup = signup;
 window.login = login;
 window.logout = logout;
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnb</h1>, root);
 });
---
+++ BenchBnB/frontend/reducers/session_reducer.js
@@ -0,0 +1,28 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS, LOGOUT }
+  from '../actions/session_actions';
+
+import { merge } from 'lodash';
+
+const _defaultState = {
+  currentUser: null,
+  errors: ["Invalid credentials"]
+};
+
+const SessionReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case (RECEIVE_CURRENT_USER):
+      let currentUser = action.currentUser;
+      return merge({}, state, currentUser);
+    case (RECEIVE_ERRORS):
+      let errors = action.errors;
+      return merge({}, state, errors);
+    case (LOGOUT):
+      return _defaultState;
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
